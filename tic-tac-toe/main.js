9398626542132e90706f2b00934b8ba826ed143d
Saegl <saegl@protonmail.com>
2020-11-04 20:06:09 +0600

    tic tac toe engine prototype

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,9 +1,26 @@
-/*
-0 = Empty cell
-1 = X
-2 = O
-*/
+"user strict";
+
+let mode = 0;
+const aiMode = 0;
+const twoPlayerMode = 1;
+let btnmode = document.getElementById("dropbtn");
+
+function aimode() {
+    mode = aiMode;
+    btnmode.innerHTML = 'Play against AI'
+}
+
+function twoplayersmode() {
+    mode = twoPlayerMode;
+    btnmode.innerHTML = 'Two players game'
+}
+
 let cellValues = [
+    /*
+    0 = Empty cell
+    1 = X
+    -1 = O
+    */
     0, 0, 0,
     0, 0, 0,
     0, 0, 0,
@@ -30,17 +47,17 @@ let winPos = [
     [2, 4, 6],
 ]
 
-function checkWin(side) {
+function checkWin(board, side) {
     for (let i = 0; i < winPos.length; i++) {
         x = winPos[i][0];
         y = winPos[i][1];
         z = winPos[i][2];
 
-        if (cellValues[x] == side && cellValues[y] == side && cellValues[z] == side) {
-            cells[x].style.backgroundColor = "lightcoral";
-            cells[y].style.backgroundColor = "lightcoral";
-            cells[z].style.backgroundColor = "lightcoral";
-            gameEnded = true;
+        if (board[x] == side && board[y] == side && board[z] == side) {
+            // cells[x].style.backgroundColor = "lightcoral";
+            // cells[y].style.backgroundColor = "lightcoral";
+            // cells[z].style.backgroundColor = "lightcoral";
+            // gameEnded = true;
             return true;
         };
     }
@@ -55,13 +72,13 @@ function addReloadButton(move) {
     move.appendChild(a);
 }
 
-function updateMove() {
-    if (checkWin(1)) {
+function updateLabel() {
+    if (checkWin(cellValues, 1)) {
         move.innerHTML = "X is win ";
         addReloadButton(move);
         return;
     }
-    if (checkWin(2)) {
+    if (checkWin(cellValues, -1)) {
         move.innerHTML = "O is win ";
         addReloadButton(move);
         return;
@@ -74,26 +91,125 @@ function updateMove() {
     }
 }
 
+function randrange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function availableMoves() {
+    let moves = [];
+    for (let i = 0; i < cellValues.length; i++) {
+        if (cellValues[i] == 0) {
+            moves.push(i)
+        }
+    }
+    return moves;
+}
+
+function isGameEnded(board) {
+    return checkWin(board, 1) || checkWin(board, -1);
+}
+
+function boardeval(board) {
+    if (checkWin(board, 1)) return 1;
+    if (checkWin(board, -1)) return -1;
+    return 0;
+}
+
+function times(s, x) {
+    let resp = "";
+    for (let i = 0; i < x; i++) {
+        resp += s;
+    }
+    return resp;
+}
+
+let h = 0;
+
+function maxi(board) {
+    if (isGameEnded(board)) {
+        h += 1;
+        return boardeval(board);
+    }
+    let legalmoves = availableMoves(board)
+    let bestmove = -10;
+
+    for (let i = 0; i < legalmoves.length; i++) {
+        let move = legalmoves[i];
+        board[move] = 1; // Make move
+        bestmove = Math.max(bestmove, mini(board));
+        board[move] = 0; // Unmake move
+    }
+    return bestmove;
+}
+
+function mini(board) {
+    if (isGameEnded(board)) {
+        h += 1;
+        return boardeval(board);
+    }
+    let legalmoves = availableMoves(board)
+    let bestmove = 10;
+
+    for (let i = 0; i < legalmoves.length; i++) {
+        let move = legalmoves[i];
+        board[move] = -1; // Make move
+        bestmove = Math.min(bestmove, maxi(board));
+        board[move] = 0; // Unmake move
+    }
+    return bestmove;
+}
+
+function aimakemove(movenumber) {
+    console.log("AI called");
+    let legalmoves = availableMoves();
+
+    let bestmovepos = -1;
+    let besteval = 1000;
+
+    for (let j = 0; j < legalmoves.length; j++) {
+        let move = legalmoves[j];
+        
+        console.log(move);
+
+        cellValues[move] = -1;
+        let neweval = maxi(cellValues, movenumber, 0);
+        cellValues[move] = 0;
+
+        console.log("move evaluation " + neweval);
+
+        if (neweval <= besteval) {
+            besteval = neweval;
+            bestmovepos = move;
+        }
+    }
+
+    console.log("engine move " + bestmovepos + " position evaluation: " + besteval);
+    cells[bestmovepos].innerHTML = "O";
+    cellValues[bestmovepos] = -1;
+    side2move = true;
+}
+
 for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', event => {
+        console.log('Button called');
         moveCount++;
         if (gameEnded || cells[i].innerHTML != "") {
             return;
         }
+
         if (side2move) {
             cells[i].innerHTML = "X";
             cellValues[i] = 1;
             side2move = false;
         } else {
             cells[i].innerHTML = "O";
-            cellValues[i] = 2;
+            cellValues[i] = -1;
             side2move = true;
         }
-        if (moveCount >= 9) {
-            move.innerHTML = "draw ";
-            addReloadButton(move);
-            return;
-        }
-        updateMove();
+
+        aimakemove(moveCount);
+        moveCount++;
+        
+        updateLabel();
     });
 }
